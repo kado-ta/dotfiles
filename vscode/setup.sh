b97b4df12fb267cc95836d2c45335a3691f37807
kado-ta <hideaki.kdt+github@gmail.com>
2026-03-23 18:03:41 +0900

    Update VSCode extensions

--- vscode/setup.sh
+++ vscode/setup.sh
@@ -7,11 +7,9 @@ fi
 
 SCRIPT_DIR=$(cd $(dirname $0) && pwd)
 VSCODE_DIR="$HOME/Library/Application Support/Code/User"
-CURSOR_DIR="$HOME/Library/Application Support/Cursor/User"
 
 echo "SCRIPT_DIR: $SCRIPT_DIR"
 echo "VSCODE_DIR: $VSCODE_DIR"
-echo "CURSOR_DIR: $CURSOR_DIR"
 
 # VSCode
 # Create symbolic link of settings.json
@@ -29,18 +27,23 @@ else
   echo "Install 'code' command from VSCode command palette to install your extensions."
 fi
 
+# NOTE: Cursor はしばらく使用しない予定なので、コマンドを無効化しておく。
+
 # Cursor
+# CURSOR_DIR="$HOME/Library/Application Support/Cursor/User"
+# echo "CURSOR_DIR: $CURSOR_DIR"
+
 # Create symbolic link of settings.json
-ln -snfv "${SCRIPT_DIR}/settings.json" "${CURSOR_DIR}/settings.json"
+# ln -snfv "${SCRIPT_DIR}/settings.json" "${CURSOR_DIR}/settings.json"
 
 # Install VSCode Extensions using the code command
-if [ "$(which code)" != "" ]; then
-  if [ "$(cursor --list-extensions)" != "$(cat "${SCRIPT_DIR}/extensions")" ]; then
-    cat < "${SCRIPT_DIR}/extensions" | while read -r line
-    do
-      code --install-extension "$line"
-    done
-  fi
-else
-  echo "Install 'cursor' command from Cursor command palette to install your extensions."
-fi
+# if [ "$(which code)" != "" ]; then
+#   if [ "$(cursor --list-extensions)" != "$(cat "${SCRIPT_DIR}/extensions")" ]; then
+#     cat < "${SCRIPT_DIR}/extensions" | while read -r line
+#     do
+#       code --install-extension "$line"
+#     done
+#   fi
+# else
+#   echo "Install 'cursor' command from Cursor command palette to install your extensions."
+# fi
